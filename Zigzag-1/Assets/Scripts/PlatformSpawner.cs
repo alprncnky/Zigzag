514e116a16baaa642a341dda2f03fa470b225cd1
Alperen Çinkaya <nerepla95@gmail.com>
2018-04-11 22:18:48 +0300

    game app

---
+++ Zigzag-1/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour {
+
+
+    public GameObject platform;        //ınstatiate yapmak icin tanimlama;
+    public GameObject diamonds;
+
+
+    Vector3 lastPos;
+    float size;
+    public bool gameOver;
+
+	// Use this for initialization
+	void Start () {
+        lastPos = platform.transform.position;
+        size = platform.transform.localScale.x;
+        for(int i=0;i<12;i++)
+        {
+            SpawnPlatforms();
+        }
+    }
+	
+    public void StartSpawningPlatforms()
+    {
+        InvokeRepeating("SpawnPlatforms", 0.1f, 0.1f);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if(GameManager.instance.gameOver==true)
+        {
+            CancelInvoke("SpawnPlatforms");
+        }
+	}
+
+
+    void SpawnPlatforms()
+    {
+        if(gameOver)
+        { return; }
+        int rand = Random.Range(0, 6);    // 0 ile 6 arasinda rastgele sayi olustur
+        if (rand < 3)
+        {
+            SpawnX();
+        }
+        else
+        {
+            SpawnZ();
+        }
+    }
+    void SpawnX()
+    {
+        Vector3 pos = lastPos;
+        pos.x += size;
+        lastPos = pos;
+        Instantiate(platform, pos,Quaternion.identity);      //  Instantiate( kopyalancak platform, nereye, dondurulcek yon )
+
+        int rand = Random.Range(0,8);       // diamondi rastgele ekleme kismi
+        if(rand==3)
+        {
+            Instantiate(diamonds, new Vector3(pos.x,pos.y+1,pos.z), diamonds.transform.rotation);                   // diamonds lar platformun icinde olustugu icin yukseltilmesi 1 yuksek olmasi lazim
+        }
+    }
+
+    void SpawnZ()
+    {
+        Vector3 pos = lastPos;
+        pos.z += size;
+        lastPos = pos;
+        Instantiate(platform, pos, Quaternion.identity);      //  Instantiate( kopyalancak platform, nereye, dondurulcek yon )
+        int rand = Random.Range(0, 8);
+        if (rand == 3)
+        {
+            Instantiate(diamonds, new Vector3(pos.x, pos.y + 1, pos.z), diamonds.transform.rotation);
+        }
+    }
+}
---
+++ Zigzag-1/Assets/Scripts/UiManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;           //unity de texti eklerken ekledik bunu scriptte kullanabilmek icin
+using UnityEngine.SceneManagement;     // button u kontrol etme
+
+public class UiManager : MonoBehaviour {
+
+    public static UiManager instance;           //bu scripteki fonksiyonlara filan baska yerlerden ulasmak icin 
+
+    public GameObject zigzagPanel;
+    public GameObject gameoverPanel;
+    public GameObject tapText;
+    public Text score;
+    public Text highScore1;
+    public Text highScore2;
+    public Text level;
+
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        highScore1.text = "High Score: "+ PlayerPrefs.GetInt("highScore").ToString();
+    }
+
+
+    public void GameStart()
+    {
+        if(!PlayerPrefs.HasKey("level"))
+            PlayerPrefs.SetInt("level", 1);
+        highScore1.text = PlayerPrefs.GetInt("highScore").ToString();
+        tapText.SetActive(false);           //oyun basladiginda tap to start yazmasin diye
+        zigzagPanel.GetComponent<Animation>().Play("PanelUp");
+
+    }
+
+    public void GameOver()
+    {
+        gameoverPanel.SetActive(true);
+        score.text = PlayerPrefs.GetInt("score").ToString();
+        highScore2.text = PlayerPrefs.GetInt("highScore").ToString();
+        if (PlayerPrefs.GetInt("score") > 99)
+            PlayerPrefs.SetInt("level", PlayerPrefs.GetInt("level") + 1);
+        level.text = PlayerPrefs.GetInt("level").ToString();
+        gameoverPanel.SetActive(true);
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
